e68f94742980f94289e8a16cbda8edc279fe859b
Moody <mrahman01@stuy.edu>
2018-10-07 12:31:11 -0400

    added shiftX, shiftY, and both types of shift (Point)

--- Driver.java
+++ Driver.java
@@ -3,6 +3,10 @@ public class Driver{
   public static void main(String[] args) {
     Point wack = new Point(3.0, 5);
 
+    wack.shift(34, 6.0);
+
     System.out.println(wack.getX());
+    System.out.println(wack.getY());
+
   }
 }
--- Point.java
+++ Point.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class Point{
 
   private double x;
@@ -13,5 +15,29 @@ public class Point{
   }
   public double getY(){
     return this.y;
+  }
+
+
+  private void shiftX(double x){
+    this.x = this.x + x;
+  }
+  private void shiftY(double y){
+    this.y = this.y + y;
+  }
+  private double degToRad(int degree){
+    return (degree * PI)/ 180;
+  }
+
+
+  public void shift(double x, double y){
+    this.x = this.x + x;
+    this.y = this.y + y;
+  }
+
+  public void shift(int angle, double dist){
+    double diffY = dist * sin(degToRad(angle));
+    double diffX = dist * cos(degToRad(angle));
+
+    shift(diffX, diffY);
   }
 }
