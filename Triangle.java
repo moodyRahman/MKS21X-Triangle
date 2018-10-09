34ab06e750b2fdc3536aa847ad5323aff454ad3d
Moody <mrahman01@stuy.edu>
2018-10-09 14:00:28 -0400

    added Triangle and basic constructors + fields

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle{
+
+  private Point p1;
+  private Point p2;
+  private Point p3;
+
+  public Triangle(x1, y1, x2, y2, x3, y3){
+
+  }
+
+  public Triangle(Point p1, Point p2, Point p3)
+}
