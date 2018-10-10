470d202538e5779288056d15e3288c8056501ca9
Moody <mrahman01@stuy.edu>
2018-10-10 13:36:58 -0400

    added constructors

--- Triangle.java
+++ Triangle.java
@@ -5,9 +5,14 @@ public class Triangle{
   private Point p3;
 
   public Triangle(x1, y1, x2, y2, x3, y3){
-
+    p1 = new Point(x1, y1);
+    p2 = new Point(x1, y1);
+    p3 = new Point(x1, y1);
   }
 
   public Triangle(Point p1, Point p2, Point p3){
-    p1 = new 
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
   }
+}
